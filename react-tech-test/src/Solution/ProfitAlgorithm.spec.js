0e48912a5ea03e5e5486f3c21d51396b3e7ef753
MonkieDragon <clivejeffries@hotmail.com>
2022-04-02 22:48:00 +0100

    solved

--- react-tech-test/src/Solution/ProfitAlgorithm.spec.js
+++ react-tech-test/src/Solution/ProfitAlgorithm.spec.js
@@ -1,23 +1,37 @@
-import {products} from "../AdditionalFiles/mockData";
+import { products } from "../AdditionalFiles/mockData";
 
 // You can use this calculateProfit function as a way of testing your algorithm,
 // just put your implementation in and the tests will pass if it is correct.
 
 // To run the test, call "npm run test" in the terminal.
 
-const calculateProfit = () => {}
+const calculateProfit = (prod) => {
+	const taxFreeQuan = Math.min(prod.quantitySold, 10);
+	const taxableQuan = Math.max(prod.quantitySold - 10, 0);
+	const taxFreeProf = taxFreeQuan * (prod.soldPrice - prod.costToBusiness);
+	const taxableProf =
+		taxableQuan * (prod.soldPrice - prod.costToBusiness) * 0.92;
+	return taxFreeProf + taxableProf;
+};
 
-describe('Tax Calculation', () => {
-  it('calculates the correct amount of tax when product is under tax threshold', () => {
-    const possibleAnswers = ['16384.464', '£16,384.464', '£16,384.46', '£16384.46', '£16384.464', 16384.464, 16384.46];
+describe("Tax Calculation", () => {
+	it("calculates the correct amount of tax when product is under tax threshold", () => {
+		const possibleAnswers = [
+			"16384.464",
+			"£16,384.464",
+			"£16,384.46",
+			"£16384.46",
+			"£16384.464",
+			16384.464,
+			16384.46,
+		];
 
-    expect(possibleAnswers).toContain(calculateProfit(products[0]))
-  })
+		expect(possibleAnswers).toContain(calculateProfit(products[0]));
+	});
 
-  it('calculates the correct amount of tax when product is over tax threshold', () => {
-    const possibleAnswers = ['833.54', '£833.54', 833.54];
+	it("calculates the correct amount of tax when product is over tax threshold", () => {
+		const possibleAnswers = ["833.54", "£833.54", 833.54];
 
-    expect(possibleAnswers).toContain(calculateProfit(products[36]))
-  })
-
-})
+		expect(possibleAnswers).toContain(calculateProfit(products[36]));
+	});
+});
--- react-tech-test/src/Solution/YourSolution.js
+++ react-tech-test/src/Solution/YourSolution.js
@@ -1,33 +1,105 @@
-import '../AdditionalFiles/App.css';
-import * as React from "react";
+import "../AdditionalFiles/App.css";
+import { useState, useEffect } from "react";
 
 //This is the API url to fetch from
-const API_URL = 'https://matchesfashion.com/api/products';
+const API_URL = "https://matchesfashion.com/api/products";
 const TAX_RATE = 0.08;
 
+const formCur = (money) => {
+	return money.toLocaleString("en-US", {
+		style: "currency",
+		currency: "GBP",
+	});
+};
+
+const calculateProfit = (prod) => {
+	const taxFreeQuan = Math.min(prod.quantitySold, 10);
+	const taxableQuan = Math.max(prod.quantitySold - 10, 0);
+	const taxFreeProf = taxFreeQuan * (prod.soldPrice - prod.costToBusiness);
+	const taxableProf =
+		taxableQuan * (prod.soldPrice - prod.costToBusiness) * 0.92;
+	return formCur(taxFreeProf + taxableProf);
+};
+
 function YourSolution() {
-    return (
-    <div className="App">
-      <table id="products">
-        <thead>
-        <tr>
-          <th>Id</th>
-          <th>Brand</th>
-          <th>Name</th>
-          <th>Quantity Sold</th>
-          <th>Sold Price</th>
-          <th>Cost To Business</th>
-        </tr>
-        </thead>
-          <tbody>
-          </tbody>
-      </table>
-      <button>First Page</button>
-      <button>Previous Page</button>
-      <button>Next Page</button>
-      <button>Last Page</button>
-    </div>
-  );
+	const [products, setProducts] = useState([]);
+	const [total, setTotal] = useState(0);
+	const [page, setPage] = useState(0);
+
+	useEffect(() => {
+		fetch(API_URL + `?page=${page}`)
+			.then((response) => response.json())
+			.then((data) => {
+				setProducts(data.products);
+				setTotal(data.count);
+				console.log(data);
+			});
+	}, [page]);
+
+	return (
+		<div className="App">
+			<table id="products">
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Brand</th>
+						<th>Name</th>
+						<th>Quantity Sold</th>
+						<th>Sold Price</th>
+						<th>Cost To Business</th>
+						<th>Profit after Tax</th>
+					</tr>
+				</thead>
+				<tbody>
+					{products.map((product, index) => {
+						return (
+							<tr key={index}>
+								<td>{product.id}</td>
+								<td>{product.brand}</td>
+								<td>{product.name}</td>
+								<td>{product.quantitySold}</td>
+								<td>{formCur(product.soldPrice)}</td>
+								<td>{formCur(product.costToBusiness)}</td>
+								<td>{calculateProfit(product)}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+			<button
+				disabled={page === 0}
+				onClick={() => {
+					setPage(0);
+				}}
+			>
+				First Page
+			</button>
+			<button
+				disabled={page === 0}
+				onClick={() => {
+					setPage((currPage) => currPage - 1);
+				}}
+			>
+				Previous Page
+			</button>
+			<button
+				onClick={() => {
+					setPage((currPage) => currPage + 1);
+				}}
+				disabled={10 * (page + 1) >= total}
+			>
+				Next Page
+			</button>
+			<button
+				disabled={10 * (page + 1) >= total}
+				onClick={() => {
+					setPage(Math.floor(total / 10) - 1);
+				}}
+			>
+				Last Page
+			</button>
+		</div>
+	);
 }
 
 export default YourSolution;
